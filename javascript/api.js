58814f4386c4e1d28f3a63413ed463445d96e75b
HeltonCarlos <heltonbritopa@gmail.com>
2021-04-04 16:46:46 -0300

    json README
    Aplicando json

--- javascript/api.js
+++ javascript/api.js
@@ -1,4 +1,17 @@
-{
-    "funcao":["Analista de sistema","Analista de Requesitos","Aux.Administrativo","Designer","Programador Front-End","Programador Back-End","UX-UI"],
-
-}
+[{
+    "funcao":["Analista de sistema",
+                "Analista de Requesitos",
+                "Aux.Administrativo",
+                "Designer",
+                "Programador Front-End",
+                "Programador full stack",
+                "Programador Back-End",
+                "UX-UI"],
+    "profissao":[0,1,2,4,5,6,7,8,9,'+ 10 anos'],
+    "pdc":["nao","sim"],
+    "deficiencia":["Deficiência Física",
+                    "Deficiência Auditiva",
+                    "Deficiência Visual",
+                    "Deficiência Mental",
+                    "Deficiência Múltipla"]
+}]
