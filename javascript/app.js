f95e329b8ec9ff6191c76cdae021e6407ca2a1a7
HeltonCarlos <heltonbritopa@gmail.com>
2021-04-04 23:11:46 -0300

    cpf README
    cpf ajuste

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,14 @@
+var cpf =document.getElementById('cpf')
+cpf.oninput= function(){
+    var campocpf= cpf.value;
+    if(campocpf.length==3||campocpf.length==7){
+        cpf.value+=".";
+    }
+    if(campocpf.length==11){
+        cpf.value+="-";
+    }
+}
+
 document.getElementById('enviaone').addEventListener('click', ()=>{
     var nome =document.getElementById('nome').value
     var cpf =document.getElementById('cpf').value
@@ -6,14 +17,10 @@ document.getElementById('enviaone').addEventListener('click', ()=>{
     var pai =document.getElementById('pai').value
     var filhos =document.getElementById('filhos').value
 
-   
+
     if(!nome || !cpf || !nascimento || !mae || !pai || !filhos){
         alert('*Preencha os campos!');
-        var red = document.querySelectorAll('input')
-        red.style.border="2px solid red";
-    }else{
-        alert('enviado')
-    }
+        return false;}
 
     document.getElementById('nome').value= ""
     document.getElementById('cpf').value= ""
