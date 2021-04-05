9d8602ade4033c949838f9391ed3ef3b6ec9209e
HeltonCarlos <heltonbritopa@gmail.com>
2021-04-05 14:36:27 -0300

    index README
    Index completa

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,5 @@
 var cpf =document.getElementById('cpf')
+var nascimento =document.getElementById('nascimento')
 cpf.oninput= function(){
     var campocpf= cpf.value;
     if(campocpf.length==3||campocpf.length==7){
@@ -9,18 +10,29 @@ cpf.oninput= function(){
     }
 }
 
+nascimento.oninput=()=>{
+    var camponas=nascimento.value;
+    if(camponas.length==2|| camponas.length==5){
+        nascimento.value+="/";
+    }
+}
+
 document.getElementById('enviaone').addEventListener('click', ()=>{
     var nome =document.getElementById('nome').value
     var cpf =document.getElementById('cpf').value
     var nascimento =document.getElementById('nascimento').value
     var mae =document.getElementById('mae').value
     var pai =document.getElementById('pai').value
-    var filhos =document.getElementById('filhos').value
+    var filhos =document.getElementById('filhos')
 
 
     if(!nome || !cpf || !nascimento || !mae || !pai || !filhos){
-        alert('*Preencha os campos!');
+        alert('*Preencha todos os campos!');
         return false;}
+
+        else{
+            location.href="parte02.html"
+        }
 
     document.getElementById('nome').value= ""
     document.getElementById('cpf').value= ""
